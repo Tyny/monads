97faf4348b401c2182e2cad23d643555f65da52b
Martin Silva <msmartinsilva@gmail.com>
2017-04-25 07:59:19 -0300

    improve coding

--- src/examples/greetings/user_greetings.js
+++ src/examples/greetings/user_greetings.js
@@ -4,10 +4,17 @@ const { dot } = require("./../../data/object");
 // Improve the use od dot when we know how to curry functions
 // .bind(dot("name"))
 
-function greetUser(user) /* Maybe<String> */ {
+/**
+ * 
+ * @param {object} user
+ * @returns string 
+ */
+
+function greetUser(user) {
   return Maybe.unit(user)
     .map(dot("name")).orElse("stranger")
-    .map((name) => `Hello ${name}`);
+    .map((name) => `Hello ${name}`).just();
 }
+
 
 module.exports = { greetUser };
--- test/examples/greetings/user_greetings.test.js
+++ test/examples/greetings/user_greetings.test.js
@@ -4,15 +4,15 @@ const { greetUser } = require("./../../../src/examples/greetings/user_greetings"
 describe("user greetings", function() {
 
   it("should say hello to user", withExpect(true, function() {
-    return greetUser({ name: "Gonza" }).just() === "Hello Gonza"; 
+    return greetUser({ name: "Gonza" }) === "Hello Gonza"; 
   }));
 
   it("should say hello to anonymous user", withExpect(true, function() {
-    return greetUser({ age: 31 }).just() === "Hello stranger"; 
+    return greetUser({ age: 31 }) === "Hello stranger"; 
   }));
 
   it("should say hello to null user", withExpect(true, function() {
-    return greetUser(null).just() === "Hello stranger"; 
+    return greetUser(null) === "Hello stranger"; 
   }));
 
 });
